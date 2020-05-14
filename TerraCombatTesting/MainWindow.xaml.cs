54aa402df232eb5ce35db55a13fdf774bacd69a2
pwbolton77 <pwbolton77@gmail.com>
2020-05-14 10:37:32 -0700

    Add project files.

---
+++ TerraCombatTesting/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using TerraCombatTesting.ViewModel;
+
+namespace TerraCombatTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            MainViewModel = new MainViewModel();
+            InitializeComponent();
+            DataContext = MainViewModel;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            //_viewModel.OnClosing(e);
+        }
+
+        public MainViewModel MainViewModel { get; set; }
+
+        private void ButonRunTrials(object sender, RoutedEventArgs e)
+        {
+            string or = MainViewModel.OffenseRating.ToString();
+            string dr = MainViewModel.DefenseRating.ToString();
+            string num_trials = MainViewModel.NumTrials.ToString();
+
+            string msg = $"OR= {or}  DR={dr}  trials={num_trials}";
+
+            MessageBox.Show(msg);
+        }
+    }
+}
---
+++ TerraCombatTesting/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraCombatTesting.ViewModel
+{
+    public class MainViewModel : Observable
+    {
+
+        private int _offenseRating;
+
+        public int OffenseRating
+        {
+            get { return _offenseRating; }
+            set 
+            { 
+                if (value != _offenseRating)
+                {
+                    _offenseRating = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int _defenseRating;
+        public int DefenseRating
+        {
+            get { return _defenseRating; }
+            set 
+            { 
+                if (value != _defenseRating)
+                {
+                    _defenseRating = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int _numTrials;
+        public int NumTrials
+        {
+            get { return _numTrials; }
+            set 
+            { 
+                if (value != _numTrials)
+                {
+                    _numTrials= value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
